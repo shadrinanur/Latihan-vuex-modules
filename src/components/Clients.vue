d360213017371448dde928a15274b2fdb81c7805
shadrinanur <nurazizahshadrina@gmail.com>
2023-08-08 14:25:56 +0700

    belajar-vuex-modules

---
+++ src/components/Clients.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label for="">Jenis Bantuan</label>
+            <select @change="$emit('selectedBantuan', $event.target.value)"
+            class="block appearance-none w-full py-1 px-2 mb-1 text-base
+            leading-normal bg-white text-gray-800 border border-gray-200 rounded">
+            <option value="">Pilih Bantuan</option>
+            <option v-for="row in listBantuan" :key="row.name"
+            :value="row.name">
+            {{ row.name }}
+          </option>
+        </select>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    computed: {
+        listBantuan() {
+            return this.$store.state.clients.listBantuan
+        }
+    }
+}
+</script>
---
+++ src/components/Donatur.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label for="">Donatur</label>
+            <select @change="$emit('selectedDonatur', $event.target.value)"
+            class="block appearance-none w-full py-1 px-2 mb-1 text-base
+            leading-normal bg-white text-gray-800 border border-gray-200 rounded">
+            <option value="">Pilih Donatur</option>
+            <option v-for="donatur in listDonatur" 
+            :key="donatur.name" :value="donatur.name">
+            {{ donatur.name }}
+          </option>
+        </select>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    computed: {
+        listDonatur() {
+            return this.$store.state.donatur.listDonatur
+        }
+    }
+}
+</script>
